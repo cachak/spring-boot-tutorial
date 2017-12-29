dc603772bd5914bcfc7a58e8275d21c31fc26f06
Sahir <cachak@myindo.co.id>
2017-12-29 22:03:29 +0700

    add redirectController

---
+++ spring-boot-mvc/spring-boot-mvc-controller/src/main/java/id/web/sahir/tutorial/springboot/mvc/controller/controller/RedirectController.java
@@ -0,0 +1,14 @@
+package id.web.sahir.tutorial.springboot.mvc.controller.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/redirect")
+public class RedirectController {
+
+    @RequestMapping(value = {"/","/landing"})
+    public String defaultAction() {
+        return "redirectDefault";
+    }
+}
